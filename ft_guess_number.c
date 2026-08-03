c61bf3ed28b177a4867ce12c01711a0407d1a02e
CloudAFK <245870771+CloudAFK@users.noreply.github.com>
2026-08-03 23:24:47 +0200

    test: test pour comprendre la notion du random

--- ft_guess_number.c
+++ ft_guess_number.c
@@ -6,7 +6,19 @@
 /*   By: romasant <romasant@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/03 23:11:46 by romasant          #+#    #+#             */
-/*   Updated: 2026/08/03 23:15:17 by romasant         ###   ########.fr       */
+/*   Updated: 2026/08/03 23:24:21 by romasant         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+int main(void)
+{
+    printf("Voici le nombre %d\n", rand() % 100 + 1);
+    printf("Voici le nombre %d\n", rand() % 100 + 1);
+    printf("Voici le nombre %d\n", rand() % 100 + 1);
+    printf("Voici le nombre %d\n", rand() % 100 + 1);
+    return (0);
+}
