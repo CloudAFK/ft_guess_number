e3ed8ce95b701222372da0ef77121cbb9da74098
CloudAFK <245870771+CloudAFK@users.noreply.github.com>
2026-08-05 12:37:43 +0200

    modif: remise en place du test

--- ft_guess_number.c
+++ ft_guess_number.c
@@ -6,7 +6,7 @@
 /*   By: romasant <romasant@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/03 23:11:46 by romasant          #+#    #+#             */
-/*   Updated: 2026/08/04 21:11:34 by romasant         ###   ########.fr       */
+/*   Updated: 2026/08/05 12:37:17 by romasant         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,7 +31,7 @@ int main(void)
 		nombre_random = rand() % 100 + 1;
 		write(1, "\033[H\033[J", 7);
 		ft_putstr("Devine le nombre entre 1 et 100 ! Tu as 10 vies.\n");
-		while (life < 0)
+		while (life > 0)
 		{
 			//printf("Nombre machine : %d\n", nombre_random);
 			flag_choice = ft_choice_user(nombre_random, life, nb_try);
